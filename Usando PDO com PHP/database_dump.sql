675767a7eb9193bfd4e0e7ed06c5585a83864a9e
Glauco Custodio <glauco.custodio@gmail.com>
2012-08-04 10:53:27 -0300

    Exclusion from old folder

--- Usando PDO com PHP/database_dump.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1
