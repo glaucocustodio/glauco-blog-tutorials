22008562341582a57239734946a5056a0021346d
Glauco Custodio <glauco.custodio@gmail.com>
2012-08-06 15:32:11 -0300

    Fix of syntax error

--- Usando PDO do PHP/database.php
+++ Usando PDO do PHP/database.php
@@ -7,7 +7,7 @@ try{
     $pdo = new PDO("mysql:host=127.0.0.1;dbname=glauco_blog_tutorials_db;", "root", "");
 }catch(PDOException $e){
     // Caso ocorra algum erro na conexão com o banco, exibe a mensagem
-    echo 'Falha ao conectar no banco de dados: '.$e->getNessage();
+    echo 'Falha ao conectar no banco de dados: '.$e->getMessage();
     die;
 }
 ?>
