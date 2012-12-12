bf7cf2d40d1bdd7aeece0758b13a76e72f4cfe61
Glauco Custodio <glauco.custodio@gmail.com>
2012-12-12 21:42:10 -0200

    Static blog inclusion

---
+++ Blog Estático Feito com Sinatra/main.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+require 'sinatra'
+require 'find'
+
+def get_files(path)
+  files = []
+  Find.find(path) do |c|
+    files << File.basename(c, ".erb") if !File.directory?(c)
+  end
+  return files
+end
+
+get '/' do
+  @posts = get_files('./views/posts/')
+  erb :index
+end
+
+get '/visualizar/:post' do
+  halt 404 unless File.exist?("views/posts/#{params[:post]}.erb")
+  erb :"posts/#{params[:post]}"
+end
+
+get '/sobre-o-autor' do
+  erb :author
+end
+
+not_found do
+  erb :"404"
+end
---
+++ Blog Estático Feito com Sinatra/views/404.erb
@@ -0,0 +1,1 @@
+<h1>Erro 404 - Página não encontrada</h1>
